0fc9243aa582b45b3de75582a0449fb952f5deac
Yong Yu En, Elvis <ELVISYONG.16@ichat.sp.edu.sg>
2018-07-18 05:17:47 +0800

    Add basic sign up and login model.

---
+++ TLTT/Models/CognitoUser.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.CognitoIdentityProvider;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TLTT.Models
+{
+    public class CognitoUser : IdentityUser
+    {
+        public string Password { get; set; }
+        public UserStatusType Status { get; set; }
+    }
+}
